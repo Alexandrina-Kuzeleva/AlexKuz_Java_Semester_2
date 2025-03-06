77e009658be89d3421c2088585c199506c4f73da
Alexandrina-Kuzeleva <alaz2005tv@gmail.com>
2025-03-06 16:24:57 +0300

    Laba 2
    Spring Project

---
+++ springAlexKuz/src/test/java/com/example/springAlexKuz/SpringAlexKuzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springAlexKuz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAlexKuzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
